14bf21e2ecf92f07dd48ac93f5c765721886584c
chaemin <w2114@e-mirim.hs.kr>
2022-11-26 00:46:02 +0900

    js 파일 분리

---
+++ section1/js/가위바위보게임.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var button = document.querySelector(".startBtn");
+    var h1 = document.getElementsByTagName("h1")[0];
+
+    var handArr = ["가위", "바위", "보"];
+
+    button.addEventListener("click", function(){
+        var num = Math.round(Math.random() * (handArr.length-1));
+        h1.innerHTML = handArr[num];
+    })
+}
