efc0eabda0bb7871808745379aa7686c523dd62f
chaemin <w2114@e-mirim.hs.kr>
2022-11-26 01:01:53 +0900

    가위바위보 게임 주석달기

--- section1/js/가위바위보게임.js
+++ section1/js/가위바위보게임.js
@@ -1,11 +1,13 @@
 window.onload = function(){
     var button = document.querySelector(".startBtn");
-    var h1 = document.getElementsByTagName("h1")[0];
+    // .startBtn 클래스가 여러개 있다면 var button = document.querySelectorAll(".startBtn")[0]; 이 방법도 가능
 
+    var h1 = document.getElementsByTagName("h1")[0];
     var handArr = ["가위", "바위", "보"];
 
-    button.addEventListener("click", function(){
+    button.addEventListener("click", function(){ //버튼 클릭 시 이벤트가 발생하게
         var num = Math.round(Math.random() * (handArr.length-1));
+        //math.round함수는 반올림 함수 handArr.length == 배열 갯수 세서하기
         h1.innerHTML = handArr[num];
     })
 }
