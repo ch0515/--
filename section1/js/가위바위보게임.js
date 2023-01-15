a4597d022a3f150be3a609e13cfe1e7b87252e65
chaemin <w2114@e-mirim.hs.kr>
2023-01-16 01:40:07 +0900

    랜덤 가위바위보 완성

--- section1/js/가위바위보게임.js
+++ section1/js/가위바위보게임.js
@@ -1,13 +1,15 @@
 window.onload = function(){
+        
     var button = document.querySelector(".startBtn");
-    // .startBtn 클래스가 여러개 있다면 var button = document.querySelectorAll(".startBtn")[0]; 이 방법도 가능
 
     var h1 = document.getElementsByTagName("h1")[0];
-    var handArr = ["가위", "바위", "보"];
 
-    button.addEventListener("click", function(){ //버튼 클릭 시 이벤트가 발생하게
-        var num = Math.round(Math.random() * (handArr.length-1));
-        //math.round함수는 반올림 함수 handArr.length == 배열 갯수 세서하기
-        h1.innerHTML = handArr[num];
+    var handArr = ["가위", "바위", "보"];
+    
+    button.addEventListener("click", function(){
+      var num = Math.round(Math.random() * (handArr.length-1)); 
+      // var num = Math.round( Math.random() * 2);
+      h1.innerHTML = handArr[num];
+      // alert(handArr[num])
     })
-}
+  }
