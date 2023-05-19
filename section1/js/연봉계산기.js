520ad328485ec278cb95f0fb184f31dfc640555b
chaemin <w2114@e-mirim.hs.kr>
2023-05-20 01:18:26 +0900

    연봉계산기 완성시키기

--- section1/js/연봉계산기.js
+++ section1/js/연봉계산기.js
@@ -1,5 +1,7 @@
 window.onload = function(){
-    var num = document.querySelector(".num");
+    var input = document.getElementsByTagName("input")[0];
+    var sec = document.querySelector(".sec");
+
     var button = document.querySelector(".startBtn");
 
     button.addEventListener("click", function(){
@@ -9,10 +11,13 @@ window.onload = function(){
     function interval(){
         clearInterval(timer); // setInterval이 중첩되지 않게 함
 
+        value = input.value * 10000;
+        secValue = value / 365 / 24 / 60 / 60; //초 계산
+
         var secNum = 0;
         timer = setInterval(function(){
             secNum ++;
-            num.innerHTML = secNum;
-        }, 10) // 1000으로 하면 1초에 한번씩 올라감
+            sec.innerHTML = "당신은 "+secNum+"초에" + (secValue * secNum).toFixed(1) + "원 벌고 있습니다.";
+        }, 1000) // 1000으로 하면 1초에 한번씩 올라감
     }
 }
